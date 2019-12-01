893042576e91f1be4a5b733264a698e661a9a996
apgearhart1 <apgearhart1@gmail.com>
2019-12-01 15:14:38 -0600

    Add sentiment values for alerts

--- gtrade/src/Data.js
+++ gtrade/src/Data.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Data.css';
 import axios from 'axios';
 
 class Data extends React.Component{
@@ -13,25 +12,44 @@ class Data extends React.Component{
         axios.get("https://protected-ravine-04165.herokuapp.com/complete/trends/" + this.state.Symbol)
         .then((response) => {
             console.log(response.data);
-            alert(response.data);
+            let nt = 'not trending';
+            let i = response.data.indexOf('+');
+            let score = response.data.slice(0, i);
+            if(response.data.includes(nt)){
+                let date = response.data.slice(i+1, response.data.length);
+                alert(this.state.Symbol + " is not trending at the moment. \nLast date it was trending is: " + date);
+            }
+            else if(score > 0 && score <= .5){
+                alert(this.state.Symbol + " outlook is looking somewhat positive, you might want to look into it!  \nReceived a score of " + score);
+            }
+            else if(score > .5 && score <= 1){
+                alert(this.state.Symbol + " outlook is very positive, we recommend you buy it soon!!  \nReceived a score of " + score);
+            }
+            else if(score < 0 && score >= -.5){
+                alert(this.state.Symbol + " outlook is looking somewhat negative, you might not want to buy it!  \nReceived a score of " + score);
+            }
+            else if(score < -.5 && score >= -1){
+                alert(this.state.Symbol + " outlook is not looking good, we don't recommend you buy it!  \nReceived a score of " + score);
+            }
+            
             this.data = response.data;
             //this.render();
             //ReactDOM.render(<div>{89}</div>);
         });
-
+        
+    }
+    
+    
+    render() {
+        
+          
+          return(
+              <h1>Your response will be in the alert box above!</h1>
+          );
+        
     }
 
     
-    render() {
-        const timer = setTimeout(() => {
-            return <h1>This is the Data screen</h1>;
-          }, 5000);
-          return () => clearTimeout(timer);
-    }
-
-    delayed() {
-        return 1;
-    }
 }
 
 export default Data;
