7489ebe8da3da13698043ec8c0e4709bc9d00e89
Nolan Blankenau <nolan@Nolans-MacBook-Pro-2.local>
2019-11-30 21:57:24 -0600

    Added one route

--- gtrade/src/Data.js
+++ gtrade/src/Data.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Data.css';
+import axios from 'axios';
+
 class Data extends React.Component{
     constructor(props){
         super(props);
         this.state ={
             Symbol: this.props.location.state.Symbol
         }
+
+        // let config = {
+        // headers: {
+        //     header1: value,
+        // }
+        // }
+        
+        // let data = {
+        // 'HTTP_CONTENT_LANGUAGE': self.language
+        // }
+        axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+        axios.get("https://protected-ravine-04165.herokuapp.com/trends/" + this.state.Symbol)
+        .then((response) => {
+            console.log(response);
+            alert(response);
+        })
         alert(this.state.Symbol);
+        //alert("1\n2");
     }
 
     
